2bbd5c5ebbf0ec211d8965fe3ea2ccbb5cef9681
ruben godde <ruben.godde@student.uhasselt.be>
2023-03-28 19:46:31 +0200

    added starting code for the HMM

--- functions.py
+++ functions.py
@@ -18,35 +18,4 @@ def generate_email():
 def generate_word():
     r = RandomWords()
     return r.get_random_word()
-
-def JPEG_compression(path, percentage):
-    # Open the image
-    image = Image.open(path)
-    substring = path.split(".jpg")[0]  # Select the second substring (index 1)
-    new_name = substring + "__jpeg" + str(percentage) + ".jpg"
-    # Compress and save the image as JPEG with quality=90
-    image.save(new_name, format='JPEG', quality=percentage)
-
-#define our sick pixelation function
-def pixelate(path, pixelation_amount):
-    # Open the image
-    image = Image.open(path)
-
-    # Get the aspect ratio of the image
-    aspect_ratio = float(image.width) / float(image.height)
-
-    # Calculate the size of the pixelated image
-    pixelated_width = int(image.width / pixelation_amount)
-    pixelated_height = int(pixelated_width / aspect_ratio)
-
-    # Resize the image to the pixelated size
-    image_small = image.resize((pixelated_width, pixelated_height), resample=Image.BOX)
-
-    # Scale the pixelated image back up to the original size
-    pixelated = image_small.resize((image.width, image.height), resample=Image.NEAREST)
-
-    # Save the pixelated image
-    substring = path.split(".jpg")[0]  
-    new_name = substring + "__pix" + str(pixelation_amount) + ".jpg"
-    pixelated.save(new_name, format='JPEG', quality=100)
-
+ 
--- main.py
+++ main.py
@@ -4,29 +4,73 @@ import string
 import os
 import glob
 import functions
-import Generate_Text_images
+import generateTextImages
+import time
+
+def generate_random(amount):
+    for i in range(amount):
+        jpegPercentage = random.choice([0,10,20,30,40,50,60,70,80,90,100])
+        pixelPercentage = random.choice([1,2,3,4,5,6,7,8,9])
+        text = functions.generate_word()
+        generateTextImages.generate_image_with_jpeg_pixelation(jpegPercentage,pixelPercentage, text)
+
+def generate_notgood():
+    #quantity
+    quantity = 100
+    paths = []
+
+    for i in range(quantity):
+        generateTextImages.generate_image(functions.generate_word())
+
+    #collect all generated images in a list
+    files = glob.glob('C:/Users/ruben/Documents/school/school 2022-2023/bachelor/Bachelor/text_images/*')
+    for f in files:
+        paths.append(f)
+
+    for path in paths:
+        jpegPercentage = random.choice([0,10,20,30,40,50,60,70,80,90,100])
+        pixelPercentage = random.choice([1,2,3,4,5,6,7,8,9])
+        generateTextImages.JPEG_compression(path, jpegPercentage)
+        generateTextImages.pixelate(path.split(".jpg")[0] + "__jpeg" + str(jpegPercentage) + ".jpg", pixelPercentage)
+        os.remove(path)
+        os.remove(path.split(".jpg")[0] + "__jpeg" + str(jpegPercentage) + ".jpg")
+
+
+#####
+#####
+##### execute when running
+#####
+#####
 
 #remove previous generated files in folder
 files = glob.glob('C:/Users/ruben/Documents/school/school 2022-2023/bachelor/Bachelor/text_images/*')
 for f in files:
     os.remove(f)
 
-Generate_Text_images.generate_image(functions.generate_word())
+start = time.time()
 
-#values = range(6)
-#for i in values:
-    #functions.JPEG_compression("text_images/" + text + ".jpg", i*20)
-    #functions.pixelate("text_images/" + text + ".jpg", i+1)
-files = glob.glob('C:/Users/ruben/Documents/school/school 2022-2023/bachelor/Bachelor/text_images/*')
-for f in files:
-    for i in range(6):
-        functions.JPEG_compression(f, i*20)
-        functions.pixelate(f, i+1)
+generate_random(2500)
+
+end = time.time()
+print(str(end - start) + " seconds")
+
+#####
+#####
+#####
+#####
+#####
+
+
+
+
 
 
 def process(line):
     print("1. " + line)
 
-#with open('C:/Users/ruben/Documents/school/school 2022-2023/bachelor/Bachelor/email_dataset_small.txt') as f:
-#    for line in f:
-#        generate_image(line)
+#read all lines in file and execute process
+def read_in_file():
+    with open('C:/Users/ruben/Documents/school/school 2022-2023/bachelor/Bachelor/email_dataset_small.txt') as f:
+        for line in f:
+            process(line)
+
